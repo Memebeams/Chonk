8cbd300d590db8731036c796c1df1a2d85d8cbca
Gerhard Tetzner <tetzner.gerhard@gmail.com>
2020-09-21 15:42:09 -0400

    Set the default maxRadius to 5.

--- src/main/java/com/memebeams/chonk/Config.java
+++ src/main/java/com/memebeams/chonk/Config.java
@@ -19,7 +19,7 @@ public class Config {
 
         SERVER_BUILDER.comment("Chunk loading settings").push(CATEGORY_CHUNK_LOADING);
         CHUNK_LOADER_MAX_RADIUS = SERVER_BUILDER.comment("Maximum radius of a world anchor in chunks (0 = 1x1, 1 = 3x3, etc.)")
-                .defineInRange("maxRadius", Integer.MAX_VALUE, 1, Integer.MAX_VALUE);
+                .defineInRange("maxRadius", 5, 1, Integer.MAX_VALUE);
         CHUNK_LOADERS_PER_PLAYER = SERVER_BUILDER.comment("Maximum number of world anchors a given player can have")
                 .defineInRange("maxAnchors", Integer.MAX_VALUE, 1, Integer.MAX_VALUE);
         SERVER_BUILDER.pop();
